a60508bf15a2e5fd38578a1dc7d2409a35ea67fd
Tyler Marien <tylermarien@users.noreply.github.com>
2023-02-02 08:16:30 -0600

    fix: Ruby 2.5 deprecated the new syntax, use new syntax (#5)

--- spec/big_decimal_inspect_spec.rb
+++ spec/big_decimal_inspect_spec.rb
@@ -1,13 +1,13 @@
 describe BigDecimal do
   describe "#inspect" do
     context "with decimal points" do
-      subject { BigDecimal.new("5000.1234").inspect }
+      subject { BigDecimal("5000.1234").inspect }
 
       it { is_expected.to eq('#<BigDecimal:"5000.1234">') }
     end
 
     context "without decimal points" do
-      subject { BigDecimal.new(5000).inspect }
+      subject { BigDecimal(5000).inspect }
 
       it { is_expected.to eq('#<BigDecimal:"5000">') }
     end
